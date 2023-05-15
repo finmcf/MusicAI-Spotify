db4e499be0a3985ca2fb699a1dee3cf6ace579a2
finmcf <92210179+finmcf@users.noreply.github.com>
2023-05-15 08:39:58 +0100

    updates

--- MusicAI-Frontend/App.tsx
+++ MusicAI-Frontend/App.tsx
@@ -1,7 +1,9 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { StyleSheet, Text, View } from "react-native";
 import * as AuthSession from "expo-auth-session";
 import axios from "axios";
+import { encode as btoa } from "base-64";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 const scopesArr = [
   "user-modify-playback-state",
@@ -34,9 +36,7 @@ const getSpotifyCredentials = async () => {
 const getAuthorizationCode = async () => {
   try {
     const credentials = await getSpotifyCredentials();
-    console.log(credentials);
     const redirectUrl = AuthSession.getRedirectUrl();
-    console.log(`Redirect URL: ${redirectUrl}`); // log the redirect URL
     const result = await AuthSession.startAsync({
       authUrl:
         "https://accounts.spotify.com/authorize" +
@@ -55,17 +55,79 @@ const getAuthorizationCode = async () => {
   }
 };
 
+const getTokens = async () => {
+  try {
+    const authorizationCode = await getAuthorizationCode();
+    const credentials = await getSpotifyCredentials();
+    const credsB64 = btoa(
+      `${credentials.clientId}:${credentials.clientSecret}`
+    );
+    const response = await fetch("https://accounts.spotify.com/api/token", {
+      method: "POST",
+      headers: {
+        Authorization: `Basic ${credsB64}`,
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+      body: `grant_type=authorization_code&code=${authorizationCode}&redirect_uri=${credentials.redirectUri}`,
+    });
+    const responseJson = await response.json();
+    const {
+      access_token: accessToken,
+      refresh_token: refreshToken,
+      expires_in: expiresIn,
+    } = responseJson;
+
+    const expirationTime = new Date().getTime() + expiresIn * 1000;
+    await setUserData("accessToken", accessToken);
+    await setUserData("refreshToken", refreshToken);
+    await setUserData("expirationTime", expirationTime.toString());
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+const setUserData = async (key, value) => {
+  try {
+    await AsyncStorage.setItem(key, value);
+  } catch (error) {
+    console.error("Failed to set user data:", error);
+  }
+};
+
+const getUserData = async (key) => {
+  try {
+    const value = await AsyncStorage.getItem(key);
+    return value;
+  } catch (error) {
+    console.error("Failed to get user data:", error);
+  }
+};
+
 export default function App() {
+  const [accessToken, setAccessToken] = useState("");
+  const [refreshToken, setRefreshToken] = useState("");
+  const [expirationTime, setExpirationTime] = useState("");
+
   useEffect(() => {
     (async () => {
-      const code = await getAuthorizationCode();
-      console.log(code);
+      await getTokens();
+
+      const accessToken = await getUserData("accessToken");
+      const refreshToken = await getUserData("refreshToken");
+      const expirationTime = await getUserData("expirationTime");
+
+      setAccessToken(accessToken);
+      setRefreshToken(refreshToken);
+      setExpirationTime(expirationTime);
     })();
   }, []);
 
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>Access Token: {accessToken}</Text>
+      <Text>Refresh Token: {refreshToken}</Text>
+      <Text>Expiration Time: {expirationTime}</Text>
     </View>
   );
 }
