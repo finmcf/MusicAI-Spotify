9cfe2a4daec28a6d2e43bb4eecbf139a88d1d0ab
finmcf <92210179+finmcf@users.noreply.github.com>
2023-05-16 09:50:12 +0100

    updates

--- MusicAI-Frontend/App.tsx
+++ MusicAI-Frontend/App.tsx
@@ -1,13 +1,30 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import LoginScreen from "./LoginScreen";
 import SettingsScreen from "./SettingsScreen";
 import MusicScreen from "./MusicScreen";
 
+import { refreshTokens, getUserData } from "./auth"; // import your functions
+
 const Stack = createStackNavigator();
 
 export default function App() {
+  useEffect(() => {
+    const refreshTokenPeriodically = async () => {
+      const refreshToken = await getUserData("refreshToken");
+      if (refreshToken) {
+        await refreshTokens();
+      }
+    };
+
+    // Refresh token every 30 minutes
+    const intervalId = setInterval(refreshTokenPeriodically, 30 * 60 * 1000);
+
+    // Clear interval on component unmount
+    return () => clearInterval(intervalId);
+  }, []);
+
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Login">
